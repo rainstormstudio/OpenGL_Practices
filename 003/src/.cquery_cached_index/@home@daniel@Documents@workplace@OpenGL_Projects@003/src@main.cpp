6fb42303cf4673b46db84d4c9706215774273daa
Daniel <rainstormstudio@yahoo.com>
2020-03-02 14:10:29 -0500

    scaling

--- 003/src/.cquery_cached_index/@home@daniel@Documents@workplace@OpenGL_Projects@003/src@main.cpp
+++ 003/src/.cquery_cached_index/@home@daniel@Documents@workplace@OpenGL_Projects@003/src@main.cpp
@@ -17,9 +17,13 @@ GLuint VAO, VBO, shader, uniformModel;
 bool direction = true;
 float triOffset = 0.0f;
 float triMaxoffset = 0.7f;
-float triIncrement = 0.0005f;
+float triIncrement = 0.0001f;
 
 float curAngle = 0.0f;
+bool sizeDirection = true;
+float curSize = 0.4f;
+float maxSize = 0.8f;
+float minSize = 0.1f;
 
 // Vertex Shader
 static const char *vShader = "                                               \n\
@@ -30,7 +34,7 @@ layout (location = 0) in vec3 pos;                                           \n\
 uniform mat4 model;                                                          \n\
                                                                              \n\
 void main() {                                                                \n\
-  gl_Position = model * vec4(0.4f * pos.x, 0.4f * pos.y, pos.z, 1.0f);       \n\
+  gl_Position = model * vec4(pos, 1.0f);                                     \n\
 }                                                                            \n\
 ";
 
@@ -187,9 +191,18 @@ int main(int argc, char *argv[])
       direction = !direction;
     }
 
-    curAngle += 0.001f;
+    curAngle += 0.0001f;
     if (curAngle >= 360) {
       curAngle -= 360;
+    }
+
+    if (sizeDirection) {
+      curSize += 0.0001f;
+    } else {
+      curSize -= 0.0001f;
+    }
+    if (curSize >= maxSize || curSize <= minSize) {
+      sizeDirection = !sizeDirection;
     }
 
     // clear window
@@ -200,8 +213,9 @@ int main(int argc, char *argv[])
 
     glm::mat4 model(1.0);
 
-    model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
     model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+    // model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+    model = glm::scale(model, glm::vec3(curSize, curSize, 1.0f));
 
     glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
     
